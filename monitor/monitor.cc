29354548ce3fd0289b00b96d917e4f953a7f3dd3
Daryl McMillan <3387416+daryl-mcmillan@users.noreply.github.com>
2020-03-10 01:29:36 +0000

    add channel indicator

--- monitor/monitor.cc
+++ monitor/monitor.cc
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
-#define BUFFER_SIZE 64*1024
+#define BUFFER_SIZE 128*1024
 
 int main() {
-	unsigned short buf[BUFFER_SIZE];
+
+	unsigned char buf[BUFFER_SIZE];
+	char last = 0x00;
+	int channel = 0;
 	for(;;) {
-		int c = fread(buf, 2, BUFFER_SIZE, stdin);
+		int len = fread(buf, 1, BUFFER_SIZE, stdin);
+		for( int i=0; i<len; i++ ) {
+			if( ( buf[i] ^ last ) & 0b00000001 ) {
+				channel = 0;
+				// sync
+			}
+			if( buf[i] & 0b00000010 ) {
+				printf( "channel %d\n", channel );
+			}
+			channel ++;
+			last = buf[i];
+		}
                	printf("%d\n", buf[0]);
 	}
 }
